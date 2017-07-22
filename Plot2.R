21e2890c4f971dc690c71d6d562ebd315075e48e
SunnySunnySunnySunny <shiqi0402@gmail.com>
2017-07-22 18:57:43 +0800

    plot2

--- Plot2.R
+++ Plot2.R
@@ -1,7 +1,6 @@
 setwd("~/Desktop/ExData_Plotting1")
 housedata <-read.table("household_power_consumption.txt", header=TRUE, sep=";", na.strings = "?")
 mydata <-rbind(housedata[housedata$Date=="1/2/2007",],housedata[housedata$Date=="2/2/2007",])
-png("plot2.png", width=480, height=480)
 mydata$Date <- as.Date(mydata$Date,format = "%d/%m/%Y")
 mydata$DateTime <- as.POSIXct(paste(mydata$Date, mydata$Time))
 png("plot2.png", width = 480, height = 480)
